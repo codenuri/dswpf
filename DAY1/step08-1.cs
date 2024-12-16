6253c9982e2c2162b0365063aac490980b9d1c55
ecourse <smkang@codenuri.co.kr>
2024-12-16 12:12:07 +0900

    Add files via upload

---
+++ DAY1/step08-1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+// Step08-1. 
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+
+    public void Foo()
+    {
+        Console.WriteLine("MainWindow Foo");
+    }
+}
+
+
+class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        this.MainWindow.Title = "Hello";
+        ((MainWindow)this.MainWindow).Foo(); 
+    }
+
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        app.Run();
+    }
+
+}
