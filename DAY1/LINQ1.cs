1162a770c21802800f0ea6ae9ceebb56b568956c
ecourse <smkang@codenuri.co.kr>
2024-12-16 16:02:43 +0900

    Add files via upload

---
+++ DAY1/LINQ1.cs
@@ -0,0 +1,12 @@
+﻿// LINQ1.cs
+
+int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var y = x.Where( x => x % 2 == 0 );
+// var y = x.Where(람다표현식);
+
+foreach(var e in y)
+    Console.WriteLine( e );
+
+
+
