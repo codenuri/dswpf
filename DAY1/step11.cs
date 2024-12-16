1981d8cd2b8a13121ca4ec8f9e340df27f469476
ecourse <smkang@codenuri.co.kr>
2024-12-16 16:39:53 +0900

    Add files via upload

--- DAY1/step11.cs
+++ DAY1/step11.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 
 // step11 : UI 는 XML 로 만들고, 
@@ -25,7 +27,19 @@ class App : Application
     {
         App app = new App();
 
-        MainWindow win = new MainWindow();
+//        MainWindow win = null;
+        Window win = null;
+
+
+        // 실행파일이 있는 위치를 기준으로 ../../.. 해야 소스가 있는 폴더 입니다.
+        FileStream fs = new FileStream("../../../ex1.txt", 
+                            FileMode.Open, FileAccess.Read);
+
+        win = (Window)XamlReader.Load(fs);
+
+        fs.Close();
+
+
         win.Show();
 
         app.Run();
