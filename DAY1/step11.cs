630d8dcce724a415de3c56ab3a6fc3a502211963
ecourse <smkang@codenuri.co.kr>
2024-12-16 16:34:29 +0900

    Add files via upload

---
+++ DAY1/step11.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+// step11 : UI 는 XML 로 만들고, 
+//          EVENT 는 C#으로 !!
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+}
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        app.Run();
+    }
+
+}
