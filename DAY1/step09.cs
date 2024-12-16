5967973c2fd899cdd4ac84cef3961963644add53
ecourse <smkang@codenuri.co.kr>
2024-12-16 12:34:54 +0900

    Add files via upload

--- DAY1/step09.cs
+++ DAY1/step09.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -13,7 +14,16 @@ class MainWindow : Window
     {
         this.Title = "Hello"; // 윈도우의 캡션 문자열에 나타납니다.
 
-        this.Content = "Hello"; // 윈도우 화면 자체에 나타납니다.
+        // #1. Content 속성 
+//      this.Content = "Hello"; // 윈도우 화면 자체에 나타납니다.
+
+
+
+        // #2. Content 에는, 문자열, 자식윈도우, 그림등 다양한것을 연결가능합니다.
+
+        Button btn = new Button();
+
+        this.Content = btn;
     }
 }
 
