c5ebfa7b68cbb637da824c693e45a3352cc4061f
ecourse <smkang@codenuri.co.kr>
2024-12-16 16:09:24 +0900

    Add files via upload

---
+++ DAY1/LINQ2.cs
@@ -0,0 +1,7 @@
+﻿int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// FirstOrDefault : 조건을 만족하는 1번째 값을 찾고, 
+//                  없을때는 디폴트값(0)을 반환해 달라.
+int v = x.FirstOrDefault( n => n % 2 == 0);
+
+Console.WriteLine(v);
