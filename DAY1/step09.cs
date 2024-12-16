e1d7acf849f80ed9488113571a0c4864f33ab3a3
ecourse <smkang@codenuri.co.kr>
2024-12-16 12:31:50 +0900

    Add files via upload

---
+++ DAY1/step09.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+// Step09. Content 라는 속성 - WPF 의 핵심
+
+
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.Title = "Hello"; // 윈도우의 캡션 문자열에 나타납니다.
+
+        this.Content = "Hello"; // 윈도우 화면 자체에 나타납니다.
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class App : Application
+{
+    public App()
+    {
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        app.Run();
+    }
+
+}
