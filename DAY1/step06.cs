874744609877bc031731aed57b7b4d8c2411eb02
ecourse <smkang@codenuri.co.kr>
2024-12-16 10:36:56 +0900

    Add files via upload

---
+++ DAY1/step06.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+// Step06. Application 도 event 가 있다.
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+}
+
+class App : Application
+{
+    // #1. 가상 메소드로 event 처리하는 코드
+    protected override void OnActivated(EventArgs e)
+    {
+        base.OnActivated(e);
+        Console.WriteLine("Program Activate 됩니다.");
+    }
+
+    public App()
+    {
+        // #2. delegate(event)를 등록하는 코드
+        // Startup event : 프로그램 처럼 실행시 발생하는 이벤트
+        this.Startup += App_Startup;
+    }
+
+    private void App_Startup(object sender, StartupEventArgs e)
+    {
+        Console.WriteLine("App_Startup");
+    }
+}
+
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        app.Run();
+    }
+}
