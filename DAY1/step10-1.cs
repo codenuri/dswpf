ee231ba7680795f55103f53183ebb21854379fe4
ecourse <smkang@codenuri.co.kr>
2024-12-16 13:21:40 +0900

    Add files via upload

--- DAY1/step10-1.cs
+++ DAY1/step10-1.cs
@@ -11,8 +11,33 @@ class MainWindow : Window
 {
     public MainWindow()
     {
+        // #1. Layout 만들고, Window의 Content 속성에 연결
         Grid grid = new Grid();
         this.Content = grid;
+
+        // #2. Row, Col 의 갯수를 지정해야 합니다.
+
+        // 일반적인 라이브러리들의 코드
+        // grid.SetRowsCols(2, 2); 
+
+        // WPF : 잘 설계된 객체지향 라이브러리 입니다.
+        //       Column, Row 라는 것도 모두 객체 입니다.
+        RowDefinition rd1 = new RowDefinition();
+        RowDefinition rd2 = new RowDefinition();
+
+        grid.RowDefinitions.Add(rd1);
+        grid.RowDefinitions.Add(rd2);
+
+        ColumnDefinition cd1 = new ColumnDefinition();
+        ColumnDefinition cd2 = new ColumnDefinition();
+
+        grid.ColumnDefinitions.Add(cd1);
+        grid.ColumnDefinitions.Add(cd2);
+
+        //------------------------------------------
+
+
+
 
         Button btn1 = new Button { Content = "button1" };
 
