08e581b885b13e5e2afec120a418e2d7f4fa3d53
ecourse <smkang@codenuri.co.kr>
2024-12-16 09:47:39 +0900

    Add files via upload

--- DAY1/step04.cs
+++ DAY1/step04.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
 
 // Step04. Window 클래스는 다양한 속성(property)를 제공합니다.
 
@@ -14,7 +15,13 @@ class Program
         win.Title = "Hello, WPF";
 
         // 윈도우 크기를 500 * 500 으로 만들어보세요.
+        win.Width = 500;
+        win.Height = 500;
+        win.Left = 100;  // x
+        win.Top = 100;   // y
 
+        win.Topmost = true;  // 최상위 윈도우(뒤로 가지 않은 윈도우)
+        win.Background = new SolidColorBrush(Colors.Red);
 
         win.Show();
 
