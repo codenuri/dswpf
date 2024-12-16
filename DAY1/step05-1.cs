9ae6d8bc28896d51852bbf63535525e217457439
ecourse <smkang@codenuri.co.kr>
2024-12-16 10:19:16 +0900

    Add files via upload

--- DAY1/step05-1.cs
+++ DAY1/step05-1.cs
@@ -5,10 +5,14 @@ using System.Windows.Media;
 // Step05-1. Window event 처리
 // #1. Window 의 파생 클래스를 만들어서 사용
 
+// 아래 코드의 의미
+// => Window 에서 발생한 이벤트는 Window 의 메소드에서 처리!!
+
 class MainWindow : Window
 {
     public MainWindow() 
     {
+        // 아래 코드에서는 "this" 는 없어도 됩니다.
         this.MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
     }
 
