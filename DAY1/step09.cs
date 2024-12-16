674a60f8d6478f2712df17f31d8b6a459b18c9ad
ecourse <smkang@codenuri.co.kr>
2024-12-16 12:37:38 +0900

    Add files via upload

--- DAY1/step09.cs
+++ DAY1/step09.cs
@@ -15,15 +15,24 @@ class MainWindow : Window
         this.Title = "Hello"; // 윈도우의 캡션 문자열에 나타납니다.
 
         // #1. Content 속성 
-//      this.Content = "Hello"; // 윈도우 화면 자체에 나타납니다.
+        //      this.Content = "Hello"; // 윈도우 화면 자체에 나타납니다.
 
 
 
         // #2. Content 에는, 문자열, 자식윈도우, 그림등 다양한것을 연결가능합니다.
+        // => Window 뿐 아니라 대부분의 UI 는 Content 속성이 있습니다.
+//      Button btn = new Button();
+//      btn.Content = "확인";
+//      this.Content = btn;
 
-        Button btn = new Button();
-
+        // #3. 위 3줄은 아래 처럼 해도 됩니다.
+        // => 객체 {} 초기화로 속성을 지정하는 코드
+        Button btn = new Button { Content = "확인" };
         this.Content = btn;
+
+        // 버튼 누를때 메세지 박스 나타나게 해보세요
+
+
     }
 }
 
