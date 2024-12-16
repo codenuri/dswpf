1223596f576b769e40bc00ec85a1aaf01d0712c6
ecourse <smkang@codenuri.co.kr>
2024-12-16 10:17:07 +0900

    Add files via upload

---
+++ DAY1/step05-1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+// Step05-1. Window event 처리
+// #1. Window 의 파생 클래스를 만들어서 사용
+
+class MainWindow : Window
+{
+    public MainWindow() 
+    {
+        this.MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
+    }
+
+    private void MainWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Click");
+    }
+}
+
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new Application();
+
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        app.Run();
+    }
+}
