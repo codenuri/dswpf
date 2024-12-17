2172992a745213200ac4512f776a469424255963
ecourse <smkang@codenuri.co.kr>
2024-12-17 16:29:01 +0900

    Add files via upload

---
+++ DAY2/06_RESOURCE/Ex4Resource4.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _06_RESOURCE
+{
+    /// <summary>
+    /// Ex4Resource4.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex4Resource4 : Window
+    {
+        public Ex4Resource4()
+        {
+            InitializeComponent();
+        }
+    }
+}
