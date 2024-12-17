46d8200d15288069b2dce49a1cebcd05904872e1
ecourse <smkang@codenuri.co.kr>
2024-12-17 12:51:04 +0900

    Add files via upload

---
+++ DAY2/03_XAML2/Ex4Window.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _03_XAML2
+{
+    /// <summary>
+    /// Ex4Window.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex4Window : Window
+    {
+        public Ex4Window()
+        {
+            InitializeComponent();
+
+            // WPF 학습할때, 각각의 컨트롤의 클래스 계층도를 가끔 보면
+            // 도움이 됩니다.
+            int n1 = 10;
+            string s = "hello";
+
+            Util.ShowHierachy(n1);
+            Util.ShowHierachy(s);
+            Util.ShowHierachy(this);
+            Util.ShowHierachy(sp);
+            Util.ShowHierachy(button);
+            Util.ShowHierachy(label);
+            Util.ShowHierachy(rect);
+
+            // App 객체의 클래스 계층도 출력되게 해보세요
+            Util.ShowHierachy(  Application.Current );
+        }
+    }
+}
---
+++ DAY2/04_LAYOUT/Ex2Canvas2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _04_LAYOUT
+{
+    /// <summary>
+    /// Ex2Canvas2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex2Canvas2 : Window
+    {
+        private Point from;
+
+        public Ex2Canvas2()
+        {
+            InitializeComponent();
+        }
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Console.WriteLine("ButtonDown");
+
+            from = e.GetPosition(this);
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point to = e.GetPosition(this);
+
+                Line line = new Line();
+                line.Stroke = new SolidColorBrush(Colors.Red);
+
+                line.X1 = from.X;
+                line.Y1 = from.Y;
+                line.X2 = to.X;
+                line.Y2 = to.Y;
+
+                // 선을 그리는 것은 결국 "Line" 객체를 만들어서
+                // canvas 의 자식으로 추가하는 것 입니다.
+                canvas.Children.Add(line);
+
+                from = to; // 현재점을 다시 시작점으로
+            }
+        }
+
+
+    }
+}
