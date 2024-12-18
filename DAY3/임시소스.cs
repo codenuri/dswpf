c691eba9de7ac29d7f976b098b59ef952f8bdcbd
ecourse <smkang@codenuri.co.kr>
2024-12-18 13:28:50 +0900

    Update 임시소스.cs

--- DAY3/임시소스.cs
+++ DAY3/임시소스.cs
@@ -1,12 +1,12 @@
-        <Button Content="button2">
+        public Ex1ElementBinding()
+        {
+            InitializeComponent();
 
-            <Button.Template>
+            Binding b = new Binding();
 
-                <ControlTemplate TargetType="Button">
-                    
-                    
-                </ControlTemplate>                
-                
-            </Button.Template>
-            
-        </Button>
+            b.Source = slider3; // slider3 의
+            b.Path = new PropertyPath("Value"); // value 를
+            b.Mode = BindingMode.OneWay;        // 한방향으로
+
+            slider4.SetBinding(System.Windows.Controls.Slider.ValueProperty, b);
+        }
