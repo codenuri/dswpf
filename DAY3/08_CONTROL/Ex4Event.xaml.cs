88c4244f8c79c2d44691a55b3075c547e818d6b3
ecourse <smkang@codenuri.co.kr>
2024-12-18 10:55:00 +0900

    Add files via upload

--- DAY3/08_CONTROL/Ex4Event.xaml.cs
+++ DAY3/08_CONTROL/Ex4Event.xaml.cs
@@ -23,5 +23,31 @@ namespace _08_CONTROL
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // #1. txtbox 의 값을 얻어야 합니다.
+            string s = txtbox.Text;
+
+            if (s == String.Empty) return;
+
+            // #2. txtbox 를 비우세요
+            txtbox.Text = "";
+
+            // #3. txtbox에서 얻은것을 listbox에 추가하세요
+            listbox.Items.Add(s);
+        }
+
+        private void listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // #1. listbox 에서 선택된 항목을 얻어야 합니다.
+            int idx = listbox.SelectedIndex; // 선택된 항목의 index
+
+            string s = listbox.SelectedItem.ToString();
+
+            // #2. label 을 변경해야 합니다.
+
+            label.Content = s;
+        }
     }
 }
