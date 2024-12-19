798363242bef77e7bdec1361c5425c90dd9acfeb
ecourse <smkang@codenuri.co.kr>
2024-12-19 12:35:34 +0900

    Add files via upload

---
+++ DAY4/12_MENU/Ex2Dialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _12_MENU
+{
+    /// <summary>
+    /// Ex2Dialog.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex2Dialog : Window
+    {
+        public Ex2Dialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MyDialog dlg = new MyDialog();
+
+            dlg.txtbox.Text = "초기값";
+
+            if (dlg.ShowDialog() == true)
+            {
+                MessageBox.Show("OK 누름");
+            }
+            else
+            {
+                MessageBox.Show("Cancel 누름");
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg =
+                                    new Microsoft.Win32.OpenFileDialog();
+
+            dlg.FileName = "디폴트파일이름";
+            dlg.DefaultExt = "*.txt";
+            dlg.Filter = "Text(*.txt)|*.txt|All(*.*)|*.*";
+
+            if (dlg.ShowDialog() == true)
+            {
+                string s = dlg.FileName; // 선택한 파일 이름
+
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
---
+++ DAY4/13_THREAD/Thread1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _13_THREAD
+{
+    /// <summary>
+    /// Thread1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Thread1 : Window
+    {
+        public Thread1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int ret = Sum(1, 1000);
+        }
+
+        // git 에 임시소스에 Sum 복사해오세요
+        public static int Sum(int first, int last)
+        {
+            int s = 0;
+            for (int i = first; i <= last; i++)
+            {
+                s += i;
+                Thread.Sleep(10);
+            }
+            return s;
+        }
+    }
+}
