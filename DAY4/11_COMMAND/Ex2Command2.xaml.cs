5473cff158b6204fc8c70e05c7d7c40c5c988330
ecourse <smkang@codenuri.co.kr>
2024-12-19 09:28:21 +0900

    Add files via upload

---
+++ DAY4/11_COMMAND/Ex2Command2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _11_COMMAND
+{
+    // 버튼에 연결한 Command 객체
+    // #1. 이명령이 선택될때(버튼 누를때) 해야할 일
+    // #2. 명령을 지금 실행할수 있는지 여부(버튼 enable 여부)
+    public class LogInCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true; // 항상 실행 할수 있다.
+            //return false; // 항상 실행할수 없다
+          
+        }
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show("Log In 진행");
+        }
+    }
+
+
+
+    public partial class Ex2Command2 : Window
+    {
+        // 아래 처럼 명령 객체를 코드로 만들어도 되지만
+        // 대부분 "XAML 의 리소스로 만들어서 사용"
+        // public static LogInCommand logincmd = new LogInCommand();
+
+        public Ex2Command2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DAY4/11_COMMAND/Ex3Command3.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _11_COMMAND
+{
+
+    public class LogInCommand2 : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        // 핵심 : 아래 함수는 언제 호출되는가 ?
+        // #1. 처음에 한번 호출됩니다.
+        // #2. 컨트롤들의 상태가 변경되어서 
+        //     다시 상태조사를 하고 싶다면 CanExecuteChanged 에 등록된
+        //     함수를 호출해야 합니다.
+        public bool CanExecute(object? parameter)
+        {
+            Console.WriteLine("CanExecute 호출됨");
+
+            Ex3Command3 win = (Ex3Command3) (Application.Current.MainWindow);
+
+            bool b1 = !string.IsNullOrEmpty(win.txtbox.Text);
+            
+            return b1 && win.checkbox.IsChecked == true;
+        }
+
+
+
+        public void FireCanExecute()
+        {
+            // CanExecute 를 다시 호출해 달라는 것
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show("Log In 진행");
+        }
+    }
+
+
+    public partial class Ex3Command3 : Window
+    {
+        public Ex3Command3()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LogInCommand2 cmd = (LogInCommand2)this.Resources["mycmd1"];
+
+            if (cmd != null)
+            {
+                cmd.FireCanExecute();
+            }
+        }
+
+        private void checkbox_Click(object sender, RoutedEventArgs e)
+        {
+            LogInCommand2 cmd = (LogInCommand2)this.Resources["mycmd1"];
+
+            if (cmd != null)
+            {
+                cmd.FireCanExecute();
+            }
+        }
+    }
+}
