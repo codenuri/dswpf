547a15fc05b8ff3bd2414ad3f7a76b2e511d2522
ecourse <smkang@codenuri.co.kr>
2024-12-19 14:01:10 +0900

    Add files via upload

---
+++ DAY4/13_THREAD/Thread3.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _13_THREAD
+{
+    /// <summary>
+    /// Thread3.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Thread3 : Window
+    {
+        public Thread3()
+        {
+            InitializeComponent();
+        }
+        public static Task<int> SumAsync(int first, int last)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+
+                int s = 0;
+                for (int i = first; i <= last; i++)
+                {
+                    s += i;
+                    Thread.Sleep(10);
+                }
+                return s;
+            });
+            return t;
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            //            Task<int> t = SumAsync(1, 500);
+            //            int n = t.Result; ; // blocking 발생.!
+
+            Console.WriteLine($"start  {Thread.CurrentThread.ManagedThreadId}");
+
+
+            int ret = await SumAsync(1, 500);
+            // #1. SumAsync 를 수행해 놓고 주스레드는
+            // #2. event 루프를 수행하고 가게 됩니다.
+            // #3. SumAsync 가 끝나면 이곳으로 와서 나머지 작업 수행
+            // => 비동기함수를 동기함수 처럼 사용
+
+            Console.WriteLine($"finish {Thread.CurrentThread.ManagedThreadId}");
+
+            label.Content = ret.ToString();
+        }
+    }
+}
---
+++ DAY4/13_THREAD/Thread4.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _13_THREAD
+{
+    /// <summary>
+    /// Thread4.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Thread4 : Window
+    {
+        public Thread4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("a.txt", FileMode.Create))
+            {
+                byte[] buff = new byte[1024 * 1024 * 2000]; // 2G
+
+                fs.Write(buff); //  동기적으로 쓰기
+
+                Console.WriteLine("WriteSync Finish");
+            }
+        }
+
+        private async void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("a.txt", FileMode.Create))
+            {
+                byte[] buff = new byte[1024 * 1024 * 2000]; // 2G
+
+//                fs.WriteAsync(buff); //  비동기적으로 쓰기
+//                               => 요청만 하고 즉시 반환
+
+                await fs.WriteAsync(buff);
+                        // 요청하고
+                        // 주스레드는 메세지처리 루프로가서 계속 메세지 처리할수 있
+                        // 작업이 종료되면 메세지Q에 종료 되었다고 event 수신
+                        // 이곳으로 다시 와서 수행
+
+
+                Console.WriteLine("WriteAsync Finish");
+            }
+        }
+    }
+}
