5d75e79df81d1391f92576304c91f80ca752c074
ecourse <smkang@codenuri.co.kr>
2024-12-19 12:44:47 +0900

    Add files via upload

--- DAY4/13_THREAD/Thread1.xaml.cs
+++ DAY4/13_THREAD/Thread1.xaml.cs
@@ -26,8 +26,25 @@ namespace _13_THREAD
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            int ret = Sum(1, 1000);
+            // #1. 직접 호출. 
+            // => 주스레드가 오랜 시간을 걸리는 작업을 하게 되면
+            // => event Q에 쌓인 event 를 처리 못하게 됩니다.
+            // => "응답없음" 발생
+            // => 나중에 한번에 이벤트 처리
+            // int ret = Sum(1, 500);  // 오랜 작업을 주스레드가 직접 호출!!
+
+            // #2. 오랜 시간이 걸리는 작업은 새로운 스레드가 하게 하고
+            // => 주스레드는 계속 이벤트처리
+
+            //Thread t = new Thread(Sum); // error. 인자 없는 함수만 가능
+
+            Thread t = new Thread( () => Sum(1, 500) ); // 람다표현식
+                           // void 함수() { Sum(1,500);} 으로생각
+            t.Start();
+
         }
+
+
 
         // git 에 임시소스에 Sum 복사해오세요
         public static int Sum(int first, int last)
@@ -37,6 +54,7 @@ namespace _13_THREAD
             {
                 s += i;
                 Thread.Sleep(10);
+                Console.WriteLine($"Sum {i}");
             }
             return s;
         }
