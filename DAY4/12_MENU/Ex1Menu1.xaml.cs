1f8633db42a6e6d76e9515d38c4e8a6a5daa09c4
ecourse <smkang@codenuri.co.kr>
2024-12-19 10:49:06 +0900

    Add files via upload

---
+++ DAY4/12_MENU/Ex1Menu1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _12_MENU
+{
+    /// <summary>
+    /// Ex1Menu1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ex1Menu1 : Window
+    {
+        public Ex1Menu1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
