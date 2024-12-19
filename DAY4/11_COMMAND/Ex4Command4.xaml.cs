e228937f3b98134091b86a93d7a757a1cdaae311
ecourse <smkang@codenuri.co.kr>
2024-12-19 09:48:36 +0900

    Add files via upload

---
+++ DAY4/11_COMMAND/Ex4Command4.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _11_COMMAND
+{
+    public class LogInCommand3 : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+ 
+        public bool CanExecute(object? parameter)
+        {
+            Console.WriteLine("CanExecute 호출됨");
+
+            Ex4Command4 win = (Ex4Command4)(Application.Current.MainWindow);
+
+            bool b1 = !string.IsNullOrEmpty(win.txtbox.Text);
+
+            return b1 && win.checkbox.IsChecked == true;
+        }
+
+
+
+        public void FireCanExecute()
+        {
+   
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show("Log In 진행");
+        }
+    }
+
+    public partial class Ex4Command4 : Window
+    {
+        public Ex4Command4()
+        {
+            InitializeComponent();
+        }
+    }
+}
