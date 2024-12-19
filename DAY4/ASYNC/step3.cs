8d29fd4972d9b14f8e5e6485b49b0de25531a562
ecourse <smkang@codenuri.co.kr>
2024-12-19 13:29:17 +0900

    Add files via upload

---
+++ DAY4/ASYNC/step3.cs
@@ -0,0 +1,36 @@
+﻿class step1
+{
+    public static Task<int> SumAsync(int first, int last)
+    {
+        Task<int> t = Task.Run(() =>
+        {
+
+            int s = 0;
+            for (int i = first; i <= last; i++)
+            {
+                s += i;
+                Thread.Sleep(10);
+                Console.WriteLine($"Sum {i}");
+            }
+            return s;
+        });
+        return t;
+    }
+
+    public static void ButtonClick()
+    {
+        Task<int> t2 = SumAsync(1, 500);
+
+        Console.WriteLine("main 은 계속 실행될수 있음");
+
+        int ret = t2.Result; 
+
+        Console.WriteLine($"결과 {ret}");
+    }
+
+    public static void Main()
+    {
+        ButtonClick();
+    }
+}
+
